f394d8a16f5f6a67bbdb6d7bc6a0786f4d086762
Tim Johnsen <timonus@gmail.com>
2023-03-28 21:32:06 -0700

    Share innards.

--- HTMLNode.m
+++ HTMLNode.m
@@ -54,99 +54,77 @@ __attribute__((objc_direct_members))
 	return @"br";
 }
 
--(void)findChildrenWithAttribute:(const char*)attribute matchingName:(const char*)className inXMLNode:(xmlNode *)node inArray:(NSMutableArray*)array allowPartial:(BOOL)partial
+-(HTMLNode*)findChildWithTag:(NSString*)tagName
 {
-	xmlNode *cur_node = NULL;
-    
-    for (cur_node = node; cur_node; cur_node = cur_node->next) 
-	{				
-		for(xmlAttrPtr attr = cur_node->properties; NULL != attr; attr = attr->next)
-		{
-			
-			if (strcmp((char*)attr->name, attribute) == 0)
-			{				
-				for(xmlNode * child = attr->children; NULL != child; child = child->next)
-				{
-                    const BOOL match = partial ? strstr((char*)child->content, className) != NULL : strcmp((char*)child->content, className) == 0;
-					if (match)
-					{
-						//Found node
-						HTMLNode * nNode = [[HTMLNode alloc] initWithXMLNode:cur_node];
-						[array addObject:nNode];
-						break;
-					}
-				}
-				break;
-			}
-		}
-		
-		[self findChildrenWithAttribute:attribute matchingName:className inXMLNode:cur_node->children inArray:array allowPartial:partial];
-	}	
-	
-}
-
--(void)findChildTags:(NSString*)tagName inXMLNode:(xmlNode *)node inArray:(NSMutableArray*)array
-{
-	xmlNode *cur_node = NULL;
-	const char * tagNameStr =  [tagName UTF8String];
-	
-	if (tagNameStr == nil)
-		return;
-	
-    for (cur_node = node; cur_node; cur_node = cur_node->next) 
-	{				
-		if (cur_node->name && strcmp((char*)cur_node->name, tagNameStr) == 0)
-		{
-			HTMLNode * node = [[HTMLNode alloc] initWithXMLNode:cur_node];
-			[array addObject:node];
-			
-		}
-		
-		[self findChildTags:tagName inXMLNode:cur_node->children inArray:array];
-	}	
+    HTMLNode *__block result;
+    findChildrenWithTag(tagName, _node->children, ^BOOL(HTMLNode *node) {
+        result = node;
+        return NO;
+    });
+    return result;
 }
 
 -(NSArray*)findChildrenWithTag:(NSString*)tagName
 {
-	NSMutableArray * array = [NSMutableArray array];
-	
-	[self findChildTags:tagName inXMLNode:_node->children inArray:array];
+	NSMutableArray *const array = [NSMutableArray array];
+    
+    findChildrenWithTag(tagName, _node->children, ^BOOL(HTMLNode *node) {
+        [array addObject:node];
+        return YES;
+    });
 	
 	return array;
 }
 
--(HTMLNode*)findChildTag:(NSString*)tagName inXMLNode:(xmlNode *)node
+static void findChildrenWithTag(NSString *tagName, xmlNode *node, BOOL (^block)(HTMLNode *node))
 {
-	xmlNode *cur_node = NULL;
-	const char * tagNameStr =  [tagName UTF8String];
-	
-    for (cur_node = node; cur_node; cur_node = cur_node->next) 
+    const char *tagNameStr =  [tagName UTF8String];
+    findChildren(^BOOL(xmlNodePtr node) {
+        return node->name && strcmp((char*)node->name, tagNameStr) == 0;
+    }, node, block);
+}
+
+static BOOL findChildren(BOOL (^matchBlock)(xmlNodePtr node), xmlNode *node, BOOL (^block)(HTMLNode *node))
+{
+    for (;node != nil; node = node->next)
 	{				
-		if (cur_node && cur_node->name && strcmp((char*)cur_node->name, tagNameStr) == 0)
+		if (matchBlock(node))
 		{
-			return [[HTMLNode alloc] initWithXMLNode:cur_node];
+            if (!block([[HTMLNode alloc] initWithXMLNode:node])) {
+                return NO;
+            }
 		}
-		
-		HTMLNode * cNode = [self findChildTag:tagName inXMLNode:cur_node->children];
-		if (cNode != NULL)
-		{
-			return cNode;
-		}
-	}	
-	
-	return NULL;
+        if (!findChildren(matchBlock, node->children, block)) {
+            return NO;
+        }
+	}
+    return YES;
 }
 
--(HTMLNode*)findChildWithTag:(NSString*)tagName
+-(NSArray*)findChildrenWithAttribute:(NSString*)attributeString matchingName:(NSString*)classString allowPartial:(BOOL)partial
 {
-	return [self findChildTag:tagName inXMLNode:_node->children];
-}
--(NSArray*)findChildrenWithAttribute:(NSString*)attribute matchingName:(NSString*)className allowPartial:(BOOL)partial
-{
-	NSMutableArray * array = [NSMutableArray array];
-
-	[self findChildrenWithAttribute:[attribute UTF8String] matchingName:[className UTF8String] inXMLNode:_node->children inArray:array allowPartial:partial];
-	
+    const char *attribute = [attributeString UTF8String];
+    const char *className = [classString UTF8String];
+	NSMutableArray *const array = [NSMutableArray array];
+    findChildren(^BOOL(xmlNodePtr node) {
+        for(xmlAttrPtr attr = node->properties; NULL != attr; attr = attr->next)
+        {
+            if (strcmp((char*)attr->name, attribute) == 0)
+            {
+                for(xmlNode * child = attr->children; NULL != child; child = child->next)
+                {
+                    if (partial ? strstr((char*)child->content, className) != NULL : strcmp((char*)child->content, className) == 0) {
+                        return YES;
+                    }
+                }
+                return NO;
+            }
+        }
+        return NO;
+    }, _node->children, ^BOOL(HTMLNode *node) {
+        [array addObject:node];
+        return YES;
+    });
 	return array;
 }
 
